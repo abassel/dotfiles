7eb96c0138a654091b154fd85deb0efae372c4ee
Alexandre Bassel <11035784+abassel@users.noreply.github.com>
2019-02-17 00:22:31 -0500

    Install sed before using it

--- boot.sh
+++ boot.sh
@@ -4,12 +4,12 @@ export _apps='tree lsof jq vim git wget crudini bash-completion git etckeeper gl
 
 function __install_all {
 
+    # Try to run in both Centos and Ubuntu
+    apt-get install -y $_apps || yum install -y $_apps
+
     # Remove timeout
     sed --in-place '/TMOUT/d' /etc/profile
 
-    # Try to run in both Centos and Ubuntu
-    apt-get install -y $_apps || yum install -y $_apps
-    
     cd ~
     wget --backups=3 https://raw.githubusercontent.com/abassel/dotfiles/master/.vimrc
     wget --backups=3 https://raw.githubusercontent.com/abassel/dotfiles/master/.bashrc
