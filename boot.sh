505a31a7f1a66a23a45a6d2cd1a4a9fea2c768c7
alexandrebassel <>
2019-02-19 20:03:09 -0500

    Configure screen utility

--- boot.sh
+++ boot.sh
@@ -14,6 +14,7 @@ function __install_all {
     wget --backups=3 https://raw.githubusercontent.com/abassel/dotfiles/master/.vimrc
     wget --backups=3 https://raw.githubusercontent.com/abassel/dotfiles/master/.bashrc
     wget --backups=3 https://raw.githubusercontent.com/abassel/dotfiles/master/.functions.sh
+    wget --backups=3 https://raw.githubusercontent.com/abassel/dotfiles/master/.screenrc
     # wget --backups=3 https://raw.githubusercontent.com/abassel/dotfiles/master/.inputrc
 
     source ~/.bashrc
