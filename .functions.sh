87ef38361918e467403cc77e9e3ca3511aa5a67e
Alexandre Bassel <11035784+abassel@users.noreply.github.com>
2020-08-29 19:29:43 -0400

    Update .functions.sh

--- .functions.sh
+++ .functions.sh
@@ -36,8 +36,15 @@ function __track_etc() {
 
 function __any_errors() {
 
-    grep -rins --include="*.log" "error" /var/log
+    grep -rins --include="*.log" "error\|warn\|critical\|fatal\|fail\|panic" /var/log
+    dmesg | egrep -i "error\|warn\|critical\|fatal\|fail\|panic"
     __end_note
+}
+
+function __update() {
+
+    curl https://raw.githubusercontent.com/abassel/dotfiles/master/boot.sh | bash
+
 }
 
 
@@ -45,6 +52,7 @@ function __help() {
 
     echo "USE-AS-IS"
     echo ""
+    echo "__update - update rc scripts"
     echo "__any_errors - grep for errors"
     echo "__wipe_all_logs - wipe all your logs"
     echo "__track_etc - git init the /etc"
