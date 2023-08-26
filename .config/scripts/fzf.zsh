31deab05e1190a87b88a2014ce46c5830cc5c3c6
Alexandre Bassel <11035784+abassel@users.noreply.github.com>
2023-08-26 14:35:28 -0400

    fixing fzf before adding repo

--- .config/scripts/fzf.zsh
+++ .config/scripts/fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/alex/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/alex/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME/.config/fzf_github/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/alex/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.config/fzf_github/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/alex/.fzf/shell/key-bindings.zsh"
+source "$HOME/.config/fzf_github/shell/key-bindings.zsh"
