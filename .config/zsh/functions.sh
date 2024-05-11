7e807cc2d39d7cb0dd55d8fc369f38450899c944
abassel <11035784+abassel@users.noreply.github.com>
2024-05-11 00:29:03 -0400

    added ai cli command to invoke NO code models only

--- .config/zsh/functions.sh
+++ .config/zsh/functions.sh
@@ -72,6 +72,24 @@ function ai_code() {
 
 }
 
+
+function ai_nocode() {
+
+    local question
+
+    # If only one parameter is specified, assume the other one is default
+    if [[ $# == 1 ]]; then
+        question=$1
+    else
+        echo "Provide only one parameter - question surrounded with quotes."
+        exit 1
+    fi
+
+    ollama list | awk 'NR>1 {print $1}' | grep -v "code" | sort -t ':' -k 2 -n | xargs -I% zsh -c "source ~/.config/zsh/.zshrc && ai % '$question'"
+
+}
+
+
 # select branch via fzf
 function __branch() {
   local tags branches target
