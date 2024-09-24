c7eef59f312b0b2b6bb00da5b5b6902cf24a0c91
Alexandre Bassel <11035784+abassel@users.noreply.github.com>
2024-09-23 22:31:05 -0400

    **chore: refactored the `ai_fabric` function to support Fabric pattern selection and input piping**
    * Make `ai_fabric` function select Fabric pattern using fzf
    * Pipe input through `fabric` command with selected pattern

--- .config/zsh/functions.sh
+++ .config/zsh/functions.sh
@@ -28,6 +28,20 @@ function ai_list_models(){
 }
 
 
+function ai_fabric() {
+
+    local piped_input
+    piped_input=$(cat)
+
+    local pattern
+    pattern=$(find ~/.config/fabric/patterns -type d -printf "%f\n" | fzf --select-1 --reverse --prompt "Select Fabric to run: " --preview-window="70%,right" --preview "bat --color=always ~/.config/fabric/patterns/{}/*")
+
+    # Run the fabric command with the selected pattern and input
+    echo "$piped_input" | fabric --pattern "$pattern"
+
+}
+
+
 function ai() {
     local model
     local question
